22807a2423ae7675017014639296d16cad63fb59
Muhammad Fahad Jamal <muhammadfahadbinjamal@gamil.com>
2026-05-20 09:30:24 +0600

    Font Change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Providers } from "./providers"; 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const josefin = Josefin_Sans({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "MediQueue – Tutor Booking System",
@@ -23,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${josefin.className} h-full antialiased`}
       suppressHydrationWarning 
     >
       <body className="min-h-full flex flex-col  bg-white dark:bg-slate-900 text-slate-900 dark:text-white transition-colors duration-300">
